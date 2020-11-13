fe8d452adc5f7198dfe3e6f0d340174baf7f6825
Belinda <belindahu@college.harvard.edu>
2020-11-13 14:32:40 -0800

    test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("let's get started!")
+
+loadData();
+
+// load data
+function loadData() {
+    d3.csv("data/netflix").then(data => {
+
+        csv = data;
+
+        console.log(csv)
+
+        matrix = new Matrix("matrix", csv, dataMarriages, dataBusiness);
+
+    });
+}
