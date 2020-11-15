8da4ccf589257247ad9b24ac4c405877339e9930
Adelson <aaguasvivas@college.harvard.edu>
2020-11-15 17:30:10 -0500

    Subscribers viz draft done

--- js/barChart.js
+++ js/barChart.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-let margin = {top: 20, right: 20, bottom: 30, left: 40},
+let margin = {top: 20, right: 20, bottom: 50, left: 70},
     width = 700 - margin.left - margin.right,
     height = 700 - margin.top - margin.bottom;
 
@@ -10,9 +10,9 @@ let x = d3.scaleBand()
 let y = d3.scaleLinear()
     .range([height, 0]);
 
-// append the svg object to the body of the page
-// append a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+let color = ["#E50914", "#00A8E1", "#b8d6e9", "#3DBB3D", "#c4302b", "#f6f3ed"]
+
+// Append svg to correct html
 let svg = d3.select("div#subscribersByPlatform.col").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -20,7 +20,7 @@ let svg = d3.select("div#subscribersByPlatform.col").append("svg")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
+// get the data, using promises
 let dataset = d3.csv("data/streamingPlatformsSubscribers.csv");
 dataset.then(function(data) {
     data.map(function(d) {
@@ -32,11 +32,11 @@ dataset.then(function(data) {
 console.log(dataset)
 
 dataset.then(function(data) {
-    // Scale the range of the data in the domains
+    // Set domains
     x.domain(data.map(function(d) { return d.platform; }));
     y.domain([0, d3.max(data, function(d) { return d.numSubscribers; })]);
 
-    // append the rectangles for the bar chart
+    // appending rectangles
     svg.selectAll(".bar")
         .data(data)
         .enter().append("rect")
@@ -46,16 +46,39 @@ dataset.then(function(data) {
         .attr("y", function(d) { return y(d.numSubscribers); })
         .attr("height", function(d) { return height - y(d.numSubscribers);
         })
-        .attr("fill", "white");
+        .attr("fill", (d, i) => {
+            return color[i % 6]
+        });
 
-    // add the x Axis
+    // add x-axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-    // add the y Axis
+    // add y-axis
     svg.append("g")
         .call(d3.axisLeft(y));
+
+    // text label for the x axis
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (width/2) + " ," +
+            (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("Platform")
+        .attr("fill", "white")
+        .attr("fontSize", 22);
+
+    // text label for the y axis
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Subscribers (in Millions)")
+        .attr("fill", "white")
+        .attr("fontSize", 22);
 
 
 })
