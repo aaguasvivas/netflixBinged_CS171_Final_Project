0fbf8f5937a2a3e33226324c3623c04c9855d561
Adelson <aaguasvivas@college.harvard.edu>
2020-11-15 18:08:59 -0500

    working on second bar chart

---
+++ js/revenueBarChart.js
@@ -0,0 +1,101 @@
+// https://www.macrotrends.net/stocks/charts/NFLX/netflix/revenue
+
+// set the dimensions and margins of the graph
+// let margin = {top: 20, right: 20, bottom: 40, left: 70},
+//     width = 700 - margin.left - margin.right,
+//     height = 700 - margin.top - margin.bottom;
+
+// set the ranges
+// let x = d3.scaleBand()
+//     .range([0, width])
+//     .padding(0.1);
+// let y = d3.scaleLinear()
+//     .range([height, 0]);
+
+
+// Append svg to correct html
+let svgRevenue = d3.select("div#revenueNetflix.col.revenue").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// get the data, using promises
+let revenueData = d3.csv("data/revenueNetflix.csv");
+revenueData.then(function(data) {
+    data.map(function(d) {
+        return d;
+    });
+});
+
+console.log("revenue", revenueData)
+
+revenueData.then(function(data) {
+    // Set domains
+    x.domain(data.map(function(d) { return d.year; }));
+    y.domain([0, d3.max(data, function(d) { return d.revenue; })]);
+
+    // appending rectangles
+    svgRevenue.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.platform); })
+        .attr("width", x.bandwidth())
+        .attr("y", function(d) { return y(d.numSubscribers); })
+        .attr("height", function(d) { return height - y(d.numSubscribers);
+        })
+        .attr("fill", (d, i) => {
+            return color[i % 6]
+        });
+
+    // add x-axis
+    svgRevenue.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add y-axis
+    svgRevenue.append("g")
+        .call(d3.axisLeft(y));
+
+    // text label for the x axis
+    svgRevenue.append("text")
+        .attr("transform",
+            "translate(" + (width/2) + " ," +
+            (height + margin.top + 20) + ")")
+        .style("text-anchor", "middle")
+        .text("Platform")
+        .attr("fill", "white")
+        .attr("fontSize", 22);
+
+    // text label for the y axis
+    svgRevenue.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Subscribers (in Millions)")
+        .attr("fill", "white")
+        .attr("fontSize", 22);
+
+    // TODO add labels to bars
+    // svg.selectAll("text")
+    //     .data(data)
+    //     .enter()
+    //     .append("text")
+    //     .text(function(d) { return d.numSubscribers; })
+    //     .attr("x", function(d){
+    //         return x(d.platform) + x.bandwidth() / 2;
+    //     })
+    //     .attr("y", function(d){
+    //         return height - y(d.numSubscribers) + 14;
+    //     })
+    //     .attr("font-family" , "sans-serif")
+    //     .attr("font-size" , 18)
+    //     .attr("fill" , "white")
+    //     .attr("text-anchor", "middle");
+
+
+})
