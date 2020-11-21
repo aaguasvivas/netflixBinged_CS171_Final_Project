d4aa0e5f3d1f06e0e84a45822146963c91b383c4
Michelle <Michelle@Michelles-Air-2.hsd1.ma.comcast.net>
2020-11-20 19:26:54 -0500

    Minor updates

--- js/main.js
+++ js/main.js
@@ -35,31 +35,3 @@ function initMainPage(data){
     MyBubbleChart = new BubbleChart('bubblechart', titles, dummy)
 
 }
-
-
-// // load data
-// function loadData() {
-//     d3.csv("data/netflix_titles.csv").then(titles => {
-//
-//         console.log(titles[0].listed_in)
-//
-//         let temp;
-//
-//         titles.forEach(function(title, index){
-//             temp = title.listed_in.split(",");
-//
-//         })
-//
-//
-//         // TODO: create list of listed_in
-//         // TODO: separate by format, then separate by listed_in
-//         // TODO: create bubbles - https://observablehq.com/@d3/bubble-chart
-//         // options - separate by genre, color by genre (tough for multiple genres) or enable filtering
-//         // refer to bechdel test
-//
-//         MyBubbleChart = new BubbleChart('bubblechart', titles);
-//
-//
-//     });
-
-//}
