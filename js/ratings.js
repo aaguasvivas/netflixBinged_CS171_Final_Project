ddccf733cb1a7bae9c7d1ee06c86de8b3e0ad00f
Michelle <Michelle@Michelles-Air-2.hsd1.ma.comcast.net>
2020-11-21 15:09:10 -0500

    Working on ratings

--- js/ratings.js
+++ js/ratings.js
@@ -2,9 +2,9 @@
 
 class Grid {
 
-    constructor(parentElement, imdbData){
+    constructor(parentElement, data){
         this.parentElement = parentElement;
-        this.imdbData = imdbData;
+        this.data = data;
 
         this.initVis();
     }
@@ -16,6 +16,8 @@ class Grid {
         vis.margin = {top: 130, right: 40, bottom: 60, left: 100};
         vis.width = $('#' + vis.parentElement).width() - vis.margin.left - vis.margin.right;
         vis.height = vis.width;
+        vis.cellHeight = 10;
+        vis.cellWidth = vis.cellHeight;
 
         // SVG drawing area
         vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -35,6 +37,31 @@ class Grid {
 
     updateVis(){
         let vis = this;
+        console.log(vis.data)
+
+        vis.sortedData = vis.data.sort(function(a,b){
+            return b[rating] - a[rating]
+        })
+
+        console.log(vis.sortedData)
+
+        // data join
+        vis.rectangle = vis.svg.selectAll("rect").data(vis.data);
+
+        // enter
+        vis.rectangle.enter()
+            .append("rect")
+            .attr("class", "rectangles")
+
+        // update
+            .merge(vis.rectangle)
+            .attr("height", vis.cellHeight)
+            .attr("width", vis.cellWidth)
+            .attr("x", (d, i) => (i * 20) + 10)
+            .attr("y", 20)
+            .attr("fill", "red")
+
+        vis.rectangle.exit().remove();
 
         vis.svg.append("rect").attr("x", 50).attr("y", 50).attr("height", 50).attr("width", 50).attr("fill", "red")
     }
