5473aee42f14908234eadf0257dca7917617b212
Michelle <Michelle@Michelles-Air-2.hsd1.ma.comcast.net>
2020-12-01 21:27:04 -0500

    switch account implemented

--- js/recommendations.js
+++ js/recommendations.js
@@ -17,32 +17,38 @@ function onClick(element){
     //show
     document.getElementById('switch').style.display = 'block';
 
-    var elements = document.getElementsByClassName(element.id);
+    let elements = document.getElementsByClassName(element.id);
     for (var i = 0; i < elements.length; i++){
         elements[i].style.display = "block";
     }
 
-    // if (element.id == "adelson"){
-    //
-    // }
-    //
-    // else if (element.id == "belinda") {
-    //
-    // }
-    //
-    // else if (element.id == "michelle"){
-    //
-    // }
-    // var topPicks = document.getElementsByClassName("top-picks");
-    // for (var i = 0; i < topPicks.length; i++){
-    //     topPicks[i].style.display = "block";
-    // }
-    //
-    // var recs = document.getElementsByClassName("recs");
-    // for (var i = 0; i < recs.length; i++){
-    //     recs[i].style.display = "block";
-    // }
+}
 
+function switchUser() {
+
+    // show
+    var text = document.getElementsByClassName("recs-text");
+    for (var i = 0; i < text.length; i++){
+        text[i].style.display = "block";
+    }
+
+    var icons = document.getElementsByClassName("icons");
+    for (var i = 0; i < icons.length; i++){
+        icons[i].style.display = "block";
+    }
+
+    // hide
+    document.getElementById('switch').style.display = 'none';
+
+    var recs = document.getElementsByClassName('recs');
+    for (var i = 0; i < recs.length; i++){
+        recs[i].style.display = "none";
+    }
+
+    var header = document.getElementsByClassName('top-picks');
+    for (var i = 0; i < header.length; i++){
+        header[i].style.display = "none";
+    }
 }
 
 
