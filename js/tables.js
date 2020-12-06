8bf34a23be68351fca7d1f44008be94d07e780b9
Adelson <aaguasvivas@college.harvard.edu>
2020-12-06 15:56:02 -0500

    final tweaks

--- js/tables.js
+++ js/tables.js
@@ -249,13 +249,13 @@ let internationalPresence = [
 var internationalPresenceTable = new Tabulator("#world_table", {
     data:internationalPresence,           //load row data from array
     layout:"fitColumns",      //fit columns to width of table
-    height:"476px",
+    height:"466px",
     responsiveLayout:"hide",  //hide columns that dont fit on the table
     tooltips:true,            //show tool tips on cells
     addRowPos:"top",          //when adding a new row, add it to the top of the table
     history:true,             //allow undo and redo actions on the table
     pagination:"local",       //paginate the data
-    paginationSize:14,         //allow 7 rows per page of data
+    paginationSize:13,         //allow 7 rows per page of data
     movableColumns:true,      //allow column order to be changed
     resizableRows:true,       //allow row order to be changed
     initialSort:[             //set the initial sort order of the data
